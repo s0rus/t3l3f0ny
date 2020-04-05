7bff36f2ec7758e9f1746239862754e02d8653f5
s0rus <molik223@gmail.com>
2020-04-05 21:21:40 +0200

    nav-update/v1.0.0
    -set up for nav
    -added logo
    -added basic styles

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css?version=1">
+    <script src="https://kit.fontawesome.com/cd3e0cd2c9.js" crossorigin="anonymous"></script>
+    <title>T3L3F0NY</title>
+</head>
+<body>
+
+    <nav class="upper-nav">
+        <section class="nav-logo">
+        <p><i class="fas fa-mobile-alt"></i> T3L3FONY</p>
+        </section>
+        <section class="nav-links">
+
+        </section>
+    </nav>
+
+</body>
+</html>
